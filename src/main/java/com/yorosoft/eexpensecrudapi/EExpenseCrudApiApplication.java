0ea55f112a34b3fc97e6a6ac60c19aee4976db27
Codeur47 <yoropapers@gmail.com>
2021-09-23 13:09:40 +0000

    First commit

---
+++ src/main/java/com/yorosoft/eexpensecrudapi/EExpenseCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.yorosoft.eexpensecrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EExpenseCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EExpenseCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yorosoft/eexpensecrudapi/EExpenseCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yorosoft.eexpensecrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EExpenseCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
